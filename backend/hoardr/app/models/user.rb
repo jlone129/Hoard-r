f10a3690b07311f4b7df1b704f85678ee78244bf
John Lonetree <john.lonetree@kin.com>
2021-10-21 10:28:02 -0500

    added bcrypt and helper to user model

--- backend/hoardr/app/models/user.rb
+++ backend/hoardr/app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
+    has_secure_password
+
     has_many :user_video_games
     has_many :video_games, through: :user_video_games
     has_many :reviews, through: :user_video_games
+    
 end
