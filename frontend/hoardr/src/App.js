0845cac7398ea47b25ac3c671419982bb12a77cc
John Lonetree <john.lonetree@kin.com>
2021-10-25 12:14:59 -0500

    added component mounting and logout functions to Frontend

--- frontend/hoardr/src/App.js
+++ frontend/hoardr/src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Homepage from './components/homepage/homepage'
+import Login from './components/login/login'
+import './App.css'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect
+} from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const urlUsers = 'http://localhost:3000/users'
+const urlReviews = 'http://localhost:3000/reviews'
+const urlLogout = 'http://localhost:3000/logout'
+
+class App extends React.Component {
+
+  state = {
+    users: [],
+    messages: [],
+    currentUser: null
+  }
+
+  componentDidMount() {
+    if(localStorage.token){
+      fetch('http://localhost:3000/user', {
+        headers: {
+          "Authorization": `Bearer ${localStorage.token}`
+        }
+      })
+      .then(res => res.json())
+      .then(user => this.setState({currentUser: user}))
+    }
+    Promise.all([
+      fetch(urlUsers),
+      fetch(urlReviews)
+    ])
+      .then(([res1, res2]) => Promise.all([res1.json(), res2.json()]))
+      .then(([users, messages]) => this.setState({
+        users: users,
+        messages: messages
+      }))
+  }
+
+  handleLogout = () => {
+
+    fetch(urlLogout, {
+      method: "DELETE",
+      headers: {
+        "Authorization": `Bearer ${localStorage.token}`
+      }
+    })
+    .then(res => res.json())
+    .then(response => {
+      if(response.message){
+        return this.setState({errorMessage: response.message})
+      }
+      localStorage.clear()
+      localStorage.removeItem("token")
+      this.setCurrentUser(null)
+      return <Redirect to="/login" />
+    })
+  }
+
+  setCurrentUser = user => {
+    this.setState({ currentUser: user })
+  }
+
+  addUser = newUser => {
+    this.setState({ users: [...this.state.users, newUser] })
+  }
+
 }
 
 export default App;
