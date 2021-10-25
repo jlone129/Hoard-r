9a1a3da681838aa9b3d76066dbc46a918554e815
John Lonetree <john.lonetree@kin.com>
2021-10-25 11:00:52 -0500

    added json rendering to the reviews controller

--- backend/hoardr/app/controllers/reviews_controller.rb
+++ backend/hoardr/app/controllers/reviews_controller.rb
@@ -1,11 +1,50 @@
 class ReviewsController < ApplicationController
 
+    skip_before_action :authorized, only: [:index, :show, :create]
+
     def index
         @reviews = Review.all
+
+        render json: @reviews, include: :user
     end
 
     def show
         @review = Review.find(params[:id])
+
+        render json: @review, include: :user
+    end
+
+    def create
+        @review = Review.create(review_params)
+        
+        if @review.valid?
+            render json: @review, include: :user
+        else
+            render json: { error: "failed to update "}, status: :not_acceptable
+        end    
+
+    end
+
+    def update
+        @review = Review.find(params[:id])
+
+        if @review.update(review_params)
+            render json: @review, include: :user
+        else
+            render json: @review.errors, status: :unprocessable_entity
+        end
+    end
+
+    def destroy
+        @review = Review.find(params[:id])
+        @review.destroy
+        render json: @review
+    end
+
+    private
+
+    def review_params
+        params.require(:reviews).permit(:title, :description, :stars, :video_game)
     end
 
 end
--- backend/hoardr/app/controllers/users_controller.rb
+++ backend/hoardr/app/controllers/users_controller.rb
@@ -32,14 +32,19 @@ class UsersController < ApplicationController
 
     def update
         @user = User.find(params[:id])
-        @user.update!(user_params)
-        redirect_to @user
+
+        if @user
+            @user.update!(user_params)
+            render json: @user, include: :reviews, status: :created
+        else
+            render json: { error: 'failed to create user' }, status: :not_acceptable
+        end
     end
 
     def destroy
         @user = User.find(params[:id])
         @user.destroy
-        redirect_to root_path
+        render json: @user
     end
 
     private
