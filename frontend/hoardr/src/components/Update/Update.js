0bfd548656b162ccfbed3eefb7b8583b16a12d29
John Lonetree <john.lonetree@kin.com>
2021-11-08 12:56:46 -0600

    Added update component, and updated login page to match bootstrap

---
+++ frontend/hoardr/src/components/Update/Update.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+
+const Update = () => (
+  <div>
+    Update Component
+  </div>
+);
+
+Update.propTypes = {};
+
+Update.defaultProps = {};
+
+export default Update;
--- frontend/hoardr/src/components/login/login.js
+++ frontend/hoardr/src/components/login/login.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { withRouter } from "react-router-dom";
+import { Container, Button, Form } from "react-bootstrap"
 
 const url = "http://localhost:3000/login"
 class Login extends Component {
@@ -52,20 +53,21 @@ class Login extends Component {
     render() {
         const {handleLogin, handleChange} = this
         return (
-            <div className="login">
-                <form className="login-form" onSubmit={handleLogin}>
-                    <div className="inline fields">
-                        <h1>Login Page</h1>
-                        <label htmlFor="username"><p>Username</p></label>
-                        <input type="text" id="username" placeholder="Username" name="username" onChange={handleChange} />
-                        <label htmlFor="password"><p>Password</p></label>
-                        <input type="password" id="password" placeholder="Password" name="password" onChange={handleChange} />
-                        <button type="submit">Login</button>
-                        <a href="/registration">First Time User</a>
-                    </div>
-                </form>
-
-                {this.state.errorMessage !== "" ? <h4 id="error-message">{this.state.errorMessage}</h4> : null}
+            <div id="login">
+                <Container className="w-25 p-3">
+                <h1>Login Page</h1>
+                {this.state.errorMessage !== "" ? <h6 id="error-message">{this.state.errorMessage}</h6> : null}
+                <Form onSubmit={handleLogin}>
+                    <Form.Group role="form" className="mb-3" controlId="formUser">
+                        <Form.Label>Username</Form.Label>
+                        <Form.Control type="text" id="username" placeholder="Username" name="username" onChange={handleChange} />
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type="password" id="password" placeholder="Password" name="password" onChange={handleChange} />
+                        <Button type="submit">Login</Button>
+                    </Form.Group>
+                </Form>
+                <a href="/registration">First Time User</a>
+                </Container>
             </div>
         )
     }
