8ce329843d9155c7bae124129176609bfd1bef3d
John Lonetree <john.lonetree@kin.com>
2021-11-09 17:10:29 -0600

    Added reviews tovideo games

--- frontend/hoardr/src/App.js
+++ frontend/hoardr/src/App.js
@@ -1,36 +1,43 @@
-import React from 'react'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Homepage from './components/Homepage/homepage'
-import Login from './components/Login/login.js'
-import Profile from './components/Profile/profile.js'
-import System from './components/System/system.js'
-import Generation from './components/Generation/Generation'
-import Genre from './components/Genre/Genre'
-import Index from './components/VideoGame/VideoGame.js'
-import Subgenre from './components/Subgenre/Subgenre'
-import Registration from './components/Registration/Registration';
-import Update from './components/Update/Update';
-import {Navbar, Container, Nav, NavDropdown, Form, FormControl, Button} from 'react-bootstrap'
-import './App.css'
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Homepage from "./components/Homepage/homepage";
+import Login from "./components/Login/login.js";
+import Profile from "./components/Profile/profile.js";
+import System from "./components/System/system.js";
+import Generation from "./components/Generation/Generation";
+import Genre from "./components/Genre/Genre";
+import Index from "./components/VideoGame/VideoGame.js";
+import Subgenre from "./components/Subgenre/Subgenre";
+import Registration from "./components/Registration/Registration";
+import Update from "./components/Update/Update";
+import {
+  Navbar,
+  Container,
+  Nav,
+  NavDropdown,
+  Form,
+  FormControl,
+  Button,
+} from "react-bootstrap";
+import "./App.css";
 import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Redirect
+  Redirect,
 } from "react-router-dom";
 
-const urlUsers = `http://localhost:3000/users/`
-const urlReviews = `http://localhost:3000/reviews/`
-const urlVideoGames = `http://localhost:3000/video_games/`
-const urlUserVideoGames = `http://localhost:3000/user_video_games/`
-const urlSystems = `http://localhost:3000/systems/`
-const urlGenerations = `http://localhost:3000/generations/`
-const urlGenres = `http://localhost:3000/genres/`
-const urlSubgenres = `http://localhost:3000/subgenres/`
-const urlLogout = `http://localhost:3000/logout`
+const urlUsers = `http://localhost:3000/users/`;
+const urlReviews = `http://localhost:3000/reviews/`;
+const urlVideoGames = `http://localhost:3000/video_games/`;
+const urlUserVideoGames = `http://localhost:3000/user_video_games/`;
+const urlSystems = `http://localhost:3000/systems/`;
+const urlGenerations = `http://localhost:3000/generations/`;
+const urlGenres = `http://localhost:3000/genres/`;
+const urlSubgenres = `http://localhost:3000/subgenres/`;
+const urlLogout = `http://localhost:3000/logout`;
 
 class App extends React.Component {
-
   state = {
     users: [],
     reviews: [],
@@ -40,19 +47,18 @@ class App extends React.Component {
     genres: [],
     subgenres: [],
     userVideoGames: [],
-    currentUser: null
-  }
+    currentUser: null,
+  };
 
   componentDidMount() {
-
-    if(localStorage.token){
-      fetch('http://localhost:3000/user', {
+    if (localStorage.token) {
+      fetch("http://localhost:3000/user", {
         headers: {
-          "Authorization": `Bearer ${localStorage.token}`
-        }
+          Authorization: `Bearer ${localStorage.token}`,
+        },
       })
-      .then(res => res.json())
-      .then(user => this.setState({currentUser: user}))
+        .then((res) => res.json())
+        .then((user) => this.setState({ currentUser: user }));
     }
     Promise.all([
       fetch(urlUsers),
@@ -63,99 +69,139 @@ class App extends React.Component {
       fetch(urlGenres),
       fetch(urlSubgenres),
     ])
-      .then(([res1, res2, res3, res4, res5, res6, res7]) => Promise.all([res1.json(), res2.json(), res3.json(), res4.json(), res5.json(), res6.json(), res7.json()]))
-      .then(([users, reviews, videoGames, systems, generations, genres, subgenres]) => this.setState({
-        users: users,
-        reviews: reviews,
-        videoGames: videoGames,
-        systems: systems,
-        generations: generations,
-        genres: genres,
-        subgenres: subgenres
-      }))
+      .then(([res1, res2, res3, res4, res5, res6, res7]) =>
+        Promise.all([
+          res1.json(),
+          res2.json(),
+          res3.json(),
+          res4.json(),
+          res5.json(),
+          res6.json(),
+          res7.json(),
+        ])
+      )
+      .then(
+        ([
+          users,
+          reviews,
+          videoGames,
+          systems,
+          generations,
+          genres,
+          subgenres,
+        ]) =>
+          this.setState({
+            users: users,
+            reviews: reviews,
+            videoGames: videoGames,
+            systems: systems,
+            generations: generations,
+            genres: genres,
+            subgenres: subgenres,
+          })
+      );
   }
 
   handleLogout = () => {
-
     fetch(urlLogout, {
       method: "DELETE",
       headers: {
-        "Authorization": `Bearer ${localStorage.token}`
-      }
+        Authorization: `Bearer ${localStorage.token}`,
+      },
     })
-    .then(res => res.json())
-    .then(response => {
-      if(response.message){
-        return this.setState({errorMessage: response.message})
-      }
-      localStorage.clear()
-      localStorage.removeItem("token")
-      this.setCurrentUser(null)
-      return <Redirect to="/login" />
-    })
-  }
+      .then((res) => res.json())
+      .then((response) => {
+        if (response.message) {
+          return this.setState({ errorMessage: response.message });
+        }
+        localStorage.clear();
+        localStorage.removeItem("token");
+        this.setCurrentUser(null);
+        return <Redirect to="/login" />;
+      });
+  };
 
-  setCurrentUser = user => {
-    this.setState({ currentUser: user })
-  }
+  setCurrentUser = (user) => {
+    this.setState({ currentUser: user });
+  };
 
-  addUser = newUser => {
-    this.setState({ users: [...this.state.users, newUser] })
-  }
+  addUser = (newUser) => {
+    this.setState({ users: [...this.state.users, newUser] });
+  };
 
-  editUser = editedUser => {
-    let userEdit = this.state.users.map( user => {
-      if(user.id === editedUser.id) {
-        return editedUser
+  editUser = (editedUser) => {
+    let userEdit = this.state.users.map((user) => {
+      if (user.id === editedUser.id) {
+        return editedUser;
       } else {
-        return user
+        return user;
       }
-    })
-    this.setState({ users: userEdit })
-  }
+    });
+    this.setState({ users: userEdit });
+  };
 
-  addReview = newReview => {
-    this.setState({ reviews: [...this.state.reviews, newReview] })
-  }
+  addReview = (newReview) => {
+    this.setState({ reviews: [...this.state.reviews, newReview] });
+  };
 
-  removeReview = delReview => {
-    this.setState({ reviews: this.state.reviews.filter(review => review.id !== delReview.id) })
-  }
+  removeReview = (delReview) => {
+    this.setState({
+      reviews: this.state.reviews.filter(
+        (review) => review.id !== delReview.id
+      ),
+    });
+  };
 
-  editReview = editedReview => {
-    let newReviews = this.state.reviews.map( review => {
-      if(review.id === editedReview.id){
-        return editedReview
+  editReview = (editedReview) => {
+    let newReviews = this.state.reviews.map((review) => {
+      if (review.id === editedReview.id) {
+        return editedReview;
       } else {
-        return review
+        return review;
       }
-    })
-    this.setState({ reviews: newReviews })
-  }
+    });
+    this.setState({ reviews: newReviews });
+  };
 
-  removeUser = delUser => {
-    this.setState({ users: this.state.users.filter(user => user.id !== delUser.id)})
-  }
+  removeUser = (delUser) => {
+    this.setState({
+      users: this.state.users.filter((user) => user.id !== delUser.id),
+    });
+  };
 
   render() {
-    const { addUser, addReview, removeReview, editReview, editUser, removeUser, handleLogout } = this
-    const { reviews, videoGames, currentUser } = this.state
+    const {
+      addUser,
+      addReview,
+      removeReview,
+      editReview,
+      editUser,
+      removeUser,
+      handleLogout,
+    } = this;
+    const { reviews, videoGames, currentUser } = this.state;
     return (
       <Router>
         <div>
           <Navbar bg="dark" variant="dark">
             <Container>
               <Navbar.Brand href="/home">Hoard-r</Navbar.Brand>
-                <Nav className="me-auto">
-                  <NavDropdown title="Video Games" id="navbarScrollingDropdown">
-                    <NavDropdown.Item href="/index">Video Game Index</NavDropdown.Item>
-                    <NavDropdown.Item href="/genre">Genres</NavDropdown.Item>
-                    <NavDropdown.Item href="/subgenre">Subgenres</NavDropdown.Item>
-                  </NavDropdown>
-                  <NavDropdown title="System" id="navbarScrollingDropdown">
-                    <NavDropdown.Item href="/generation">Generation</NavDropdown.Item>
-                    <NavDropdown.Item href="/system">System</NavDropdown.Item>
-                  </NavDropdown>
+              <Nav className="me-auto">
+                <NavDropdown title="Video Games" id="navbarScrollingDropdown">
+                  <NavDropdown.Item href="/video_games">
+                    Video Game Index
+                  </NavDropdown.Item>
+                  <NavDropdown.Item href="/genre">Genres</NavDropdown.Item>
+                  <NavDropdown.Item href="/subgenre">
+                    Subgenres
+                  </NavDropdown.Item>
+                </NavDropdown>
+                <NavDropdown title="System" id="navbarScrollingDropdown">
+                  <NavDropdown.Item href="/generation">
+                    Generation
+                  </NavDropdown.Item>
+                  <NavDropdown.Item href="/system">System</NavDropdown.Item>
+                </NavDropdown>
               </Nav>
               <Form className="d-flex" id="search-bar">
                 <FormControl
@@ -167,21 +213,29 @@ class App extends React.Component {
                 <Button variant="outline-success">Search</Button>
               </Form>
               <Nav>
-                <NavDropdown title="User Menu" id="input-group-dropdown-2" align="end">
-                  {currentUser &&
+                <NavDropdown
+                  title="User Menu"
+                  id="input-group-dropdown-2"
+                  align="end"
+                >
+                  {currentUser && (
                     <NavDropdown.Item href="/profile">Profile</NavDropdown.Item>
-                  }
-                  {currentUser ?
-                    <NavDropdown.Item href="/update">Update User</NavDropdown.Item>
-                  :
-                    <NavDropdown.Item href="/registration">Register</NavDropdown.Item>
-                  }
+                  )}
+                  {currentUser ? (
+                    <NavDropdown.Item href="/update">
+                      Update User
+                    </NavDropdown.Item>
+                  ) : (
+                    <NavDropdown.Item href="/registration">
+                      Register
+                    </NavDropdown.Item>
+                  )}
                   <span onClick={this.handleLogout}>
-                    {currentUser ?
+                    {currentUser ? (
                       <NavDropdown.Item href="/login">Logout</NavDropdown.Item>
-                    :
+                    ) : (
                       <NavDropdown.Item href="/login">Login</NavDropdown.Item>
-                    }
+                    )}
                   </span>
                 </NavDropdown>
               </Nav>
@@ -192,42 +246,68 @@ class App extends React.Component {
             <Route exact path="/">
               <Homepage />
             </Route>
-            <Route exact path="/index">
-              {videoGames.map(videoGame => <Index addReview={addReview} editReview={editReview} removeReview={removeReview} videoGame={videoGame} />)}
+            <Route exact path="/video_games">
+              {React.Children.toArray(
+                videoGames.map((videoGame) => (
+                  <Index
+                    reviews={reviews}
+                    addReview={addReview}
+                    editReview={editReview}
+                    removeReview={removeReview}
+                    videoGame={videoGame}
+                  />
+                ))
+              )}
             </Route>
             <Route exact path="/system">
-              {this.state.systems.map(system => <System system={system} />)}
+              {React.Children.toArray(this.state.systems.map((system) => (
+                <System system={system} />
+              )))}
             </Route>
             <Route exact path="/generation">
-              {this.state.generations.map(gen => <Generation gen={gen} />)}
+              {React.Children.toArray(this.state.generations.map((gen) => (
+                <Generation gen={gen} />
+              )))}
             </Route>
             <Route exact path="/genre">
-              {this.state.genres.map(genre => <Genre genre={genre} />)}
+              {React.Children.toArray(this.state.genres.map((genre) => (
+                <Genre genre={genre} />
+              )))}
             </Route>
             <Route exact path="/subgenre">
-              {this.state.subgenres.map(subgenre => <Subgenre subgenre={subgenre} />)}
+              {React.Children.toArray(this.state.subgenres.map((subgenre) => (
+                <Subgenre subgenre={subgenre} />
+              )))}
             </Route>
             <Route exact path="/profile">
-              <Profile user={currentUser} reviews={reviews} removeUser={removeUser} videoGames={videoGames} logout={handleLogout}/>
+              <Profile
+                user={currentUser}
+                reviews={reviews}
+                removeUser={removeUser}
+                videoGames={videoGames}
+                logout={handleLogout}
+              />
             </Route>
-            {currentUser ?
+            {currentUser ? (
               <Route exact path="/update">
                 <Update editUser={editUser} user={currentUser} />
               </Route>
-            :
+            ) : (
               <Route exact path="/registration">
-                <Registration addUser={addUser} setCurrentUser={this.setCurrentUser}/>
+                <Registration
+                  addUser={addUser}
+                  setCurrentUser={this.setCurrentUser}
+                />
               </Route>
-            }
+            )}
             <Route exact path="/login">
-              <Login setCurrentUser={this.setCurrentUser}/>
+              <Login setCurrentUser={this.setCurrentUser} />
             </Route>
           </Switch>
         </div>
       </Router>
-    )
+    );
   }
-
 }
 
 export default App;
--- frontend/hoardr/src/components/VideoGame/VideoGame.js
+++ frontend/hoardr/src/components/VideoGame/VideoGame.js
@@ -5,13 +5,42 @@ import { withRouter } from "react-router-dom";
 // const videoGameURL = `http://localhost:3000/video_games/`
 
 class Index extends Component {
+
+    getReview = () => {
+        const { videoGame, reviews } = this.props
+        return React.Children.toArray(reviews.map(review => {
+            let today = new Date( Date.now() )
+            let oneDay = 1000 * 60 * 60 * 24
+            let updated = new Date(review.updated_at)
+            let timeDifference = Math.abs(today - updated)
+            let dayDifference = Math.ceil(timeDifference / oneDay)
+            console.log(review.video_game.id)
+            console.log(videoGame.id)
+            if(review.video_game.id === videoGame.id) {
+                return (
+                    <>
+                        <Card.Body>
+                            <Card.Title>{review.title}</Card.Title>
+                            <Card.Text>{review.description}</Card.Text>
+                        </Card.Body>
+                        <Card.Footer>
+                            <small className="text-muted">Last Updated: {dayDifference} days ago</small>
+                        </Card.Footer>
+                    </>
+                );
+            }
+        }))
+    }
+
     render() {
-        const { videoGame } = this.props
-        return (
+        const { children, videoGame, reviews } = this.props
+
+
+        return(
             <div>
                 <Container>
                     <h1>Video Game Library</h1>
-                    <Card style={{ width: '16rem' }}>
+                    <Card style={{ width: '18rem' }}>
                         <Card.Img variant="top" src={videoGame.img_url} alt={videoGame.title} />
                         <Card.Body>
                             <Card.Title>{videoGame.title}</Card.Title>
@@ -21,13 +50,16 @@ class Index extends Component {
                             <ListGroupItem><b>System:</b> {videoGame.system.name}</ListGroupItem>
                             <ListGroupItem><b>Genre:</b> {videoGame.genre.name}</ListGroupItem>
                         </ListGroup>
+                        <Card.Header><b>Reviews</b></Card.Header>
+                        {this.getReview() || "Not Rendered Yet"}
                         <Card.Body>
-                            <Button varient="primary">Add to Collection</Button>
+                                <Button varient="primary">Add to Collection</Button>
                         </Card.Body>
                     </Card>
                 </Container>
             </div>
         )
+        
     }
 }
 
