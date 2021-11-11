ba66f620ec206066ec3fd647c29066a338079719
John Lonetree <john.lonetree@kin.com>
2021-11-10 23:46:36 -0600

    Rendered in review form

--- frontend/hoardr/src/components/Review/Review.js
+++ frontend/hoardr/src/components/Review/Review.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Container, Form, Button, CloseButton, Card } from 'react-bootstrap';
+import { Container, Form, Button, CloseButton, Card, Carousel, Alert } from 'react-bootstrap';
 import { withRouter } from 'react-router';
 
 class Review extends Component {
@@ -7,9 +7,51 @@ class Review extends Component {
     constructor() {
         super();
 
-        this.state={
+        this.state = {
+            title: "",
+            description: "",
+            stars: 0,
+            video_game: {},
+            user: {},
+            created: false,
             edit: false
         }
+    }
+
+    getReview = () => {
+        const { videoGame, reviews, currentUser, handleDeleteReview } = this.props
+
+        return React.Children.toArray(reviews.map(review => {
+            let today = new Date( Date.now() )
+            let oneDay = 1000 * 60 * 60 * 24
+            let updated = new Date(review.updated_at)
+            let timeDifference = Math.abs(today - updated)
+            let dayDifference = Math.ceil(timeDifference / oneDay)
+
+            if(review.video_game.id === videoGame.id) {
+                return (
+                    <>
+                        <Carousel fade>
+                            <Carousel.Item>
+                                <Card.Body>
+                                    {currentUser.id === review.user.id ?
+                                        <CloseButton id="close-button" onClick={handleDeleteReview}/>
+                                    :
+                                        <CloseButton id="close-button" disabled />
+                                    }
+                                    <Card.Text><Card.Img src={review.user.img_url} id="review-pic" /><b>{review.user.username}</b></Card.Text>
+                                    <Card.Title><b>{review.title}</b></Card.Title>
+                                    <Card.Text>{review.description}</Card.Text>
+                                </Card.Body>
+                                <Card.Footer>
+                                    <small className="text-muted">Last Updated: {dayDifference} days ago</small>
+                                </Card.Footer>
+                            </Carousel.Item>
+                        </Carousel>
+                    </>
+                );
+            }
+        }))
     }
 
     editToggle = () => {
@@ -21,17 +63,38 @@ class Review extends Component {
         this.setState({ [e.target.name]: e.target.value })
     }
 
-    handleDeleteReview = () => {
-        fetch(`http://localhost:3000/reviews/${this.props.review.id}`, {
-            method: 'DELETE',
+    handleAddReview = (e) => {
+        e.preventDefault();
+        const {
+            title,
+            description,
+            stars
+        } = this.state;
+
+        fetch(`http://localhost:3000/reviews/`, {
+            method: 'POST',
             headers: {
+                "Content-Type": "application/json",
+                Accept: "application/json",
                 "Authorization": `Bearer ${localStorage.token}`
-            }
+            },
+            body: JSON.stringify({
+                review: {
+                    "title": title,
+                    "description": description,
+                    "stars": stars,
+                    "video_game": this.props.videoGame,
+                    "user": this.props.currentUser,
+                }
+            }),
         })
-        .then(res => res.json())
-        .then(() => {
-            this.props.removeReview(this.props.review)
-            alert("Message successfully removed")
+        .then((res) => res.json())
+        .then((newReview) => {
+            this.props.addReview(newReview)
+            if(newReview.status === "created") {
+                this.setState({created: true})
+                return(<Alert>Review successfully added</Alert>)
+            }
         })
     }
 
@@ -57,44 +120,70 @@ class Review extends Component {
         })
     }
 
-    render() {
+    editReviewForm = () => {
 
-        const { handleEditReview, editToggle, handleDeleteReview, handleChange } = this
-        const { review } = this.props
-        let showEdit;
+        let { reviews, handleEditReview } = this.props
+        let { handleChange } = this
 
-        function editReviewForm() {
+        return React.Children.toArray(reviews.map( review =>{
             return(
-                <Container className="w-25 p-3">
+                <Container className="w-100 p-3">
                     <Form onSubmit={handleEditReview}>
-                        <Form.Group role="form" className="mb-3"></Form.Group>
+                        <Form.Group role="form" className="mb-3" id="editReviewForm">
                             <Form.Label>Title</Form.Label>
-                            <Form.Control type="text" id="title" name="title"></Form.Control>
+                            <Form.Control 
+                                type="text" 
+                                id="title" 
+                                name="title" 
+                                defaultValue={review.title} 
+                                onChange={handleChange} />
                             <Form.Label>Description</Form.Label>
-                            <Form.Control type="text" id="description" name="description"></Form.Control>
+                            <Form.Control 
+                                type="text" 
+                                id="description" 
+                                name="description" 
+                                defaultValue={review.description} 
+                                onChange={handleChange} />
                             <Form.Label>Stars</Form.Label>
-                            <Form.Control type="integer" id="stars" name="stars"></Form.Control>
+                            <Form.Control 
+                                type="integer" 
+                                id="stars" 
+                                name="stars" 
+                                defaultValue={review.stars} 
+                                onChange={handleChange} />
                             <Button type="submit">Update Review</Button>
+                        </Form.Group>
                     </Form>
                 </Container>
             )
-        }
+        }))
+    }
 
-        if(this.state.edit) {
-            showEdit = editReviewForm()
-        } else {
-            showEdit = null
-        }
+    //Allows current user to see the review edit form
+    toggleButton = () => {
+        
+        let { editToggle } = this
+
+        return React.Children.toArray(this.props.reviews.map(review => {
+            if(review.user.id === this.props.currentUser.id) {
+                return ( <Button onClick={editToggle}>Edit Review</Button> )
+            } else {
+                return( <Button disabled>Edit Review</Button> )
+            }
+        }))
+    }
+
+    render() {
+
+        const { getReview, toggleButton, editReviewForm} = this
+        const { edit } = this.state
 
         return (
-            <div>
-                <h1>Review Controller</h1>
-                <Card>
-                    <CloseButton onClick={handleDeleteReview}>Delete</CloseButton>
-                    <Card.Header></Card.Header>
-                    <Button onClick={showEdit}>Edit</Button>
-                </Card>
-            </div>
+            <>
+                {getReview()}   
+                {toggleButton()}
+                { edit === true ? editReviewForm() : null }
+            </>
         )
     }
 }
